a9e39cd2e0d1580af54c7ae249bc6b3e791d86bf
Nat <nat.weerawan@gmail.com>
2012-05-10 15:55:07 +0700

    Add bootstrap data.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
+import th.co.opendream.cashcard.Member
+
 class BootStrap {
 
     def init = { servletContext ->
+    	def m1 = new Member(identificationNumber:"1159900100015", firstname:"Nat", lastname: "Weerawan", telNo: "111111111", gender: "MALE", address: "Opendream")
+    	def m2 = new Member(identificationNumber: "1234567891234", firstname: "Noomz", lastname: "Siriwat", telNo: "111111111", gender: "MALE", address: "Opendream2")
+
+    	m1.save()
+    	m2.save()
     }
     def destroy = {
     }

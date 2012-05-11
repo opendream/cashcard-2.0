08e477bb2260c5c43599946bd381dcbbdfde726e
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-05-11 18:07:32 +0700

    [CASHCARD-84] Add Contract

---
+++ grails-app/domain/th/co/opendream/cashcard/Contract.groovy
@@ -0,0 +1,15 @@
+package th.co.opendream.cashcard
+
+class Contract {
+	String code
+	LoanType loanType
+	BigDecimal loanAmount = 0.000000
+	BigDecimal interestRate = 0.00
+	BigDecimal loanBalance = 0.000000
+	Boolean approvalStatus
+	Boolean loanReceiveStatus
+
+    static constraints = {
+    	code nullable: false, blank: false, unique: true
+    }
+}

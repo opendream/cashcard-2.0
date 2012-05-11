665a982f709353fb10af2fc05d2316b85b1ce482
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-05-11 18:06:49 +0700

    [CASHCARD-85] Minor refactor

--- test/unit/th/co/opendream/cashcard/LoanTypeTests.groovy
+++ test/unit/th/co/opendream/cashcard/LoanTypeTests.groovy
@@ -34,18 +34,18 @@ class LoanTypeTests {
 
         loanType.validate([field])
         assertEquals "Name must fail null validation.",
-            "nullable", loanType.errors['name']
+            "nullable", loanType.errors[field]
 
         loanType.name = ""
         loanType.validate([field])
         assertEquals "Name must fail blank validation.",
-            "blank", loanType.errors['name']
+            "blank", loanType.errors[field]
 
         def existingLoanType = generateValidLoanType("Existing Loan").save()
         loanType.name = "Existing Loan"
         loanType.validate([field])
         assertEquals "Name must fail unique validation.",
-            "unique", loanType.errors['name']
+            "unique", loanType.errors[field]
 
         loanType.name = "Common Loan"
         assertTrue "Name ${loanType.name} must pass all validations.",

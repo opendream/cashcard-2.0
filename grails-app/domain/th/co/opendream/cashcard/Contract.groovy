c41b7e44d7154a0551adbe2bc58dc3b7dd6176ab
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-05-15 10:56:05 +0700

    [Contract] Finish domain and tests.

--- grails-app/domain/th/co/opendream/cashcard/Contract.groovy
+++ grails-app/domain/th/co/opendream/cashcard/Contract.groovy
@@ -6,8 +6,8 @@ class Contract {
 	BigDecimal loanAmount = 0.000000
 	BigDecimal interestRate = 0.00
 	BigDecimal loanBalance = 0.000000
-	Boolean approvalStatus
-	Boolean loanReceiveStatus
+	Boolean approvalStatus = false
+	Boolean loanReceiveStatus = false
 
     static constraints = {
     	code nullable: false, blank: false, unique: true

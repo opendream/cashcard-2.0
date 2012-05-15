170647c5a034ab38920201fea7bd573ff753c90d
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-05-15 18:07:08 +0700

    Update Bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,5 @@
 import th.co.opendream.cashcard.Member
+import th.co.opendream.cashcard.LoanType
 
 class BootStrap {
 
@@ -8,7 +9,19 @@ class BootStrap {
 
     	m1.save()
     	m2.save()
+
+    	generateLoanType()
     }
     def destroy = {
+    }
+
+    def generateLoanType() {
+    	new LoanType(name: "เงินกู้สามัญ").save()
+    	new LoanType(name: "เงินกู้เพื่อการศึกษา").save()
+    	new LoanType(name: "เงินกู้ซื้อยานพาหนะ").save()
+    	new LoanType(name: "เงินกู้ซื้อทอง").save()
+    	new LoanType(name: "เงินกู้ซื้เครื่องใช้ไฟฟ้า").save()
+    	new LoanType(name: "เงินกู้โดยอสังหาริมทรัพย์").save()
+    	new LoanType(name: "เงินกู้โดยใช้ทรัพย์สินจำนอง").save()
     }
 }

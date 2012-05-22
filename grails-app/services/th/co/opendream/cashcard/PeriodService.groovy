97131d267042aa8712c54b3a71275f36c51e69b6
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-05-22 11:26:07 +0700

    [CASHCARD-91] PeriodService : Add calculateFormulaOne()

--- grails-app/services/th/co/opendream/cashcard/PeriodService.groovy
+++ grails-app/services/th/co/opendream/cashcard/PeriodService.groovy
@@ -22,4 +22,86 @@ class PeriodService {
             isNotNull('dueDate')
         }[0]
     }
+
+    def calculateInterestFormulaOne(period, date) {
+        date = date ? date : new Date()
+
+        def contract = period.contract
+
+        def cal = new GregorianCalendar()
+        def isLeap = cal.isLeapYear(date.year + 1900)
+        def yearDivider = isLeap ? 366 : 365
+
+        def lastPeriod = period
+        if (period.no != 1) {
+            def c = Period.createCriteria()
+            lastPeriod = c.get {
+                eq('contract', contract)
+                eq('no', period.no - 1)
+            }
+        }
+
+        def dayFromLastPeriod = 0
+        if (period.no == 1) {
+            dayFromLastPeriod = date - contract.approvalDate
+        }
+        else {
+            dayFromLastPeriod = date - lastPeriod.payoffDate
+        }
+
+        def actualInterest = contract.loanBalance * (contract.interestRate / 100 / yearDivider) * dayFromLastPeriod
+        def effectedInterest = contract.loanBalance * (0.18 / yearDivider) * dayFromLastPeriod
+        def fee = 0.00
+
+        actualInterest = actualInterest.setScale(6, BigDecimal.ROUND_HALF_UP)
+        effectedInterest = effectedInterest.setScale(6, BigDecimal.ROUND_HALF_UP)
+        if (effectedInterest < actualInterest) {
+            fee = actualInterest - effectedInterest
+        }
+
+        [
+            actualInterest: actualInterest,
+            effectedInterest: effectedInterest,
+            fee: fee
+        ]
+    }
+
+    def periodPayoff(period, amount, fine, isShareCapital, date) {
+        def periodInterest = calculateInterestFormulaOne(period, date)
+
+        def receiveTx = new ReceiveTransaction()
+
+        if (amount >= periodInterest.actualInterest) {
+            amount -= periodInterest.actualInterest
+        }
+        else {
+            throw new Exception("Amount not enough for this period.")
+        }
+        
+        if (amount > 0.00) {
+            receiveTx.balancePaid = amount
+        }
+
+        receiveTx.period = period
+        receiveTx.balanceForward = period.contract.loanBalance
+        receiveTx.interestRate = period.contract.interestRate
+        receiveTx.interestPaid = periodInterest.effectedInterest
+        receiveTx.fee = periodInterest.fee
+        receiveTx.fine = fine
+        receiveTx.isShareCapital = isShareCapital
+        receiveTx.save()
+
+        period.payoffDate = date
+        period.payoffStatus = true
+        period.save()
+
+        def contract = period.contract
+        contract.loanBalance -= receiveTx.balancePaid
+        if (contract.loanBalance < 0.00) {
+            contract.loanBalance = 0.00
+        }
+        contract.save()
+
+        receiveTx
+    }
 }
--- test/unit/th/co/opendream/cashcard/PeriodServiceTests.groovy
+++ test/unit/th/co/opendream/cashcard/PeriodServiceTests.groovy
@@ -5,12 +5,50 @@ package th.co.opendream.cashcard
 import grails.test.mixin.*
 import org.junit.*
 
+import groovy.time.TimeCategory
+
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(PeriodService)
 @Mock([Member, Contract, Period, LoanType])
 class PeriodServiceTests {
+
+    void setUpPeriod() {
+        def loanType = new LoanType(name: 'Common')
+        loanType.save()
+
+        def member = new Member(identificationNumber:"1159900100015", firstname:"Nat", lastname: "Weerawan", telNo: "111111111", gender: "MALE", address: "Opendream")
+        member.utilService = [
+            check_id_card: { id -> true }
+        ]
+        member.save()
+
+        def contract = new Contract(
+            id: 1,
+            code: "ก.55-1000-20",
+            member: member,
+            loanType: loanType,
+            loanAmount: 2000.00,
+            interestRate: 24.00,
+            loanBalance: 2000.00,
+            approvalStatus: false,
+            loanReceiveStatus: false,
+            guarantor1: "Keng",
+            guarantor2: "Neung",
+            numberOfPeriod: 3
+        )
+        contract.save()
+
+        mockDomain(Period, [
+            [id: 1, contract: contract, amount: 706.00, no: 1,
+             dueDate: new Date().plus(10), status: true, payoffStatus: true],
+            [id: 2, contract: contract, amount: 706.00, no: 2,
+             dueDate: new Date().plus(20), status: true, payoffStatus: false],
+            [id: 3, contract: contract, amount: 708.00, no: 3,
+             dueDate: new Date().plus(30), status: true, payoffStatus: false]
+        ])
+    }
 
     void testGeneratePeriod() {
         
@@ -33,44 +71,14 @@ class PeriodServiceTests {
     }
 
     void testGetCurrentPeriod() {
-        def loanType = new LoanType(name: 'Common')
-        loanType.save()
+        setUpPeriod()
 
-        def member = new Member(identificationNumber:"1159900100015", firstname:"Nat", lastname: "Weerawan", telNo: "111111111", gender: "MALE", address: "Opendream")
-        member.utilService = [
-            check_id_card: { id -> true }
-        ]
-        member.save()
-
-        def contract = new Contract(
-            code: "ก.55-1000-20",
-            member: member,
-            loanType: loanType,
-            loanAmount: 2000.00,
-            interestRate: 2.00,
-            loanBalance: 0.00,
-            approvalStatus: false,
-            loanReceiveStatus: false,
-            guarantor1: "Keng",
-            guarantor2: "Neung",
-            numberOfPeriod: 3
-        )
-        contract.save()
-
-        mockDomain(Period, [
-            [id: 1, contract: contract, amount: 300.00, no: 1,
-             dueDate: new Date().plus(10), status: true, payoffStatus: true],
-            [id: 2, contract: contract, amount: 300.00, no: 2,
-             dueDate: new Date().plus(20), status: true, payoffStatus: false],
-            [id: 3, contract: contract, amount: 300.00, no: 3,
-             dueDate: new Date().plus(30), status: true, payoffStatus: false]
-        ])
+        def contract = Contract.get(1)
 
         assert service.getCurrentPeriod(contract) == Period.get(2)
 
         def p1 = Period.get(1)
         p1.dueDate = null
-        p1.validate()
         p1.save()
 
         def p2 = Period.get(2)
@@ -82,5 +90,52 @@ class PeriodServiceTests {
         p3.save()
 
         assert service.getCurrentPeriod(contract) == null
+    }
+
+    void testCalculateFormulaOne() {
+        setUpPeriod()
+
+        def p1 = Period.get(1)
+        p1.dueDate = new Date().parse("yyyy-MM-dd", "2012-04-01")
+        p1.payoffStatus = true
+        p1.payoffDate = p1.dueDate
+        p1.save()
+
+        def p2 = Period.get(2)
+        p2.dueDate = new Date().parse("yyyy-MM-dd", "2012-05-01")
+        p2.save()
+
+        def p3 = Period.get(3)
+        p3.dueDate = new Date().parse("yyyy-MM-dd", "2012-06-01")
+        p3.save()
+
+        PeriodService.metaClass.getCurrentPeriod = { contract -> p2 }
+
+        /*
+         *********************************************************************
+         * Loan Amount : 2000.00, Interest Rate : 24% /year
+         * ================================================
+         * First month :
+         * -- balance : 2000.00
+         * -- interest : 39.344262
+         * -- effected interest : 29.508197
+         * -- fee : 9.836065
+         * ================================================
+         * Next month :
+         * -- balance : 1333.344262
+         * -- interest : 26.229723
+         * -- effected interest : 19.672292, fix grails bug to 19.672293
+         * -- fee : 6.557431, fix grails bug to 6.557430
+         *********************************************************************
+        */
+
+        def contract = Contract.get(1)
+        contract.loanBalance = 1333.344262
+        contract.save()
+
+        def result = service.calculateInterestFormulaOne(p2, p2.dueDate)
+        assert result.actualInterest == 26.229723
+        assert result.effectedInterest == 19.672293
+        assert result.fee == 6.557430
     }
 }

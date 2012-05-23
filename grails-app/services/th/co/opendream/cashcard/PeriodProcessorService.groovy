732ea8b4c81b67e1eed084111041e93acbccf895
Nat <nat.weerawan@gmail.com>
2012-05-23 19:46:58 +0700

    Move code to processor.

--- grails-app/services/th/co/opendream/cashcard/PeriodProcessorService.groovy
+++ grails-app/services/th/co/opendream/cashcard/PeriodProcessorService.groovy
@@ -1,8 +1,57 @@
 package th.co.opendream.cashcard
 
 class PeriodProcessorService {
+	def interestProcessorService
 
-    def serviceMethod() {
+    def process(Period period, amount, fine, isShareCapital, date) {
+        def processorName = period.contract.loanType.processor.toLowerCase()
+        this."$processorName"(period, amount, fine, isShareCapital, date)
+    }
 
+    def effective(Period period, amount, fine, isShareCapital, date) {
+        def actualPaymentAmount = amount
+        def periodInterest = interestProcessorService.process(period, date)
+
+        def receiveTx = new ReceiveTransaction()
+
+        if (amount >= periodInterest.actualInterest) {
+            amount -= periodInterest.actualInterest
+        }
+        else {
+            throw new Exception("Amount not enough for this period.")
+        }
+
+        receiveTx.amount = actualPaymentAmount
+        receiveTx.sign = 1
+        receiveTx.period = period
+        receiveTx.balanceForward = period.contract.loanBalance
+        receiveTx.interestRate = period.contract.interestRate
+        receiveTx.interestPaid = periodInterest.effectedInterest
+        receiveTx.fee = periodInterest.fee
+        receiveTx.fine = fine
+        receiveTx.isShareCapital = isShareCapital
+
+        period.payoffDate = date
+        period.payoffStatus = true
+        period.save()
+
+        def contract = period.contract
+        if (amount > 0.00) {
+            if (contract.loanBalance >= amount) {
+                receiveTx.balancePaid = amount
+                contract.loanBalance -= receiveTx.balancePaid
+                amount = 0.00
+            }
+            else {
+                amount -= contract.loanBalance
+                receiveTx.balancePaid = contract.loanBalance
+                contract.loanBalance = 0.00
+            }
+        }
+        receiveTx.differential = amount
+        receiveTx.save()
+        contract.save()
+
+        receiveTx
     }
 }

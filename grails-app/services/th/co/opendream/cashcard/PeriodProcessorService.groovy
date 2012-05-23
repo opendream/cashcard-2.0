4e40508f72d03c738b72129e20bfcae05fe96abc
Nat <nat.weerawan@gmail.com>
2012-05-23 19:10:05 +0700

    Add period processor.

---
+++ grails-app/services/th/co/opendream/cashcard/PeriodProcessorService.groovy
@@ -0,0 +1,8 @@
+package th.co.opendream.cashcard
+
+class PeriodProcessorService {
+
+    def serviceMethod() {
+
+    }
+}
---
+++ test/unit/th/co/opendream/cashcard/PeriodProcessorServiceTests.groovy
@@ -0,0 +1,17 @@
+package th.co.opendream.cashcard
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(PeriodProcessorService)
+class PeriodProcessorServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}

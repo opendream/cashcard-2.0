79ba54e485c03dd9ce906aeb8320008758838320
Nat <nat.weerawan@gmail.com>
2012-05-23 14:11:53 +0700

    Add field processor.

--- grails-app/domain/th/co/opendream/cashcard/LoanType.groovy
+++ grails-app/domain/th/co/opendream/cashcard/LoanType.groovy
@@ -2,8 +2,10 @@ package th.co.opendream.cashcard
 
 class LoanType {
 	String name
+	String processor
 
     static constraints = {
     	name nullable: false, blank: false, unique: true
+    	processor nullable: false, blank: false, inList: ["Effective", "Hybrid", "Flat"]
     }
 }

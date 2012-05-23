fe1dfb2e24f14617627bd0a900c5dab133e6820c
Nat <nat.weerawan@gmail.com>
2012-05-23 15:36:22 +0700

    Move periodService/testCalculateFormulaOne -> processorService/EffectiveMethod.

--- test/unit/th/co/opendream/cashcard/ProcessorServiceTests.groovy
+++ test/unit/th/co/opendream/cashcard/ProcessorServiceTests.groovy
@@ -166,4 +166,70 @@ class ProcessorServiceTests {
         assert result.effectedInterest == 19.691643
         assert result.fee == 6.563879
     }
+
+    void testCalculateEffectiveMethodNotLeapYear() {
+        /*
+         *********************************************************************
+         * Loan Amount : 2000.00, Interest Rate : 24% /year
+         * ================================================
+         * First month :
+         * -- balance : 2000.00
+         * -- interest : 40.767123 (groovy: 40.767120)
+         * -- effected interest : 30.575342 (groovy: 30.575343)
+         * -- fee : 10.191781 (groovy: 10.191777)
+         * ================================================
+         * Next month :
+         * -- balance : 1334.767123
+         * -- interest : 26.329653 (groovy: 26.329651)
+         * -- effected interest : 19.747240 (groovy: 19.747240)
+         * -- fee : 6.582413, (groovy: 6.582411)
+         *********************************************************************
+        */
+
+        setUpPeriod()
+
+        def contract = Contract.get(1)
+        contract.approvalStatus = true
+        contract.approvalDate = new Date().parse("yyyy-MM-dd", "2011-03-01")
+        contract.loanReceiveStatus = true
+        contract.payloanDate = contract.approvalDate
+        contract.save()
+
+        def p1 = Period.get(1)
+        p1.dueDate = new Date().parse("yyyy-MM-dd", "2011-04-01")
+        p1.save()
+
+        /**
+         * Test first period.
+         */
+        def result = service.process(p1, p1.dueDate)
+        assert result.actualInterest == 40.767120
+        assert result.effectedInterest == 30.575343
+        assert result.fee == 10.191777
+
+        contract.loanBalance = 1334.767123
+        contract.save()
+
+        p1.payoffStatus = true
+        p1.payoffDate = p1.dueDate
+        p1.save()
+
+        def p2 = Period.get(2)
+        p2.dueDate = new Date().parse("yyyy-MM-dd", "2011-05-01")
+        p2.save()
+
+        def p3 = Period.get(3)
+        p3.dueDate = new Date().parse("yyyy-MM-dd", "2011-06-01")
+        p3.save()
+
+        PeriodService.metaClass.getCurrentPeriod = { it -> p2 }
+
+        /**
+         * Test second period.
+         */
+        result = service.process(p2, p2.dueDate)
+        assert result.actualInterest == 26.329651
+        assert result.effectedInterest == 19.747240
+        assert result.fee == 6.582411
+    }
 }

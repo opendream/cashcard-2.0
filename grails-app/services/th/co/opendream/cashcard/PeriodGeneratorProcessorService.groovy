7329b31cf7cd9ce89c6b8560ba39a20824b01eed
Nat <nat.weerawan@gmail.com>
2012-05-28 13:26:29 +0700

    Add effective method.

--- grails-app/services/th/co/opendream/cashcard/PeriodGeneratorProcessorService.groovy
+++ grails-app/services/th/co/opendream/cashcard/PeriodGeneratorProcessorService.groovy
@@ -3,6 +3,14 @@ package th.co.opendream.cashcard
 class PeriodGeneratorProcessorService {
 
     def serviceMethod() {
+    def effective(amount, numberOfPeriod, intRatePerYear) {
+        amount = amount + (intRatePerYear/12)
 
+        def amountPerPeriod = (int)(amount / numberOfPeriod),
+            remain = ((int)amount) % numberOfPeriod
+
+        (0..<numberOfPeriod).collect { id ->
+            new Period(amount: amountPerPeriod + (id == numberOfPeriod - 1 ? remain : 0), no: id + 1)
+        }
     }
 }
--- test/unit/th/co/opendream/cashcard/PeriodGeneratorProcessorServiceTests.groovy
+++ test/unit/th/co/opendream/cashcard/PeriodGeneratorProcessorServiceTests.groovy
@@ -9,7 +9,18 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(PeriodGeneratorProcessorService)
+@Mock([LoanType, Member, Period, Contract])
 class PeriodGeneratorProcessorServiceTests {
+/*
+    void testEffective() {
+        def amount = 2000,
+            numberOfPeriod = 3;
+        def periodList = service.effective(amount, numberOfPeriod)
+        periodList.each {
+            println it.dump()
+        }
+    }
+    */
 
     void testSomething() {
         fail "Implement me"

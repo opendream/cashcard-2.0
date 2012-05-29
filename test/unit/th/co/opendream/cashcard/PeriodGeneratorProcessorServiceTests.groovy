ccfda87dd31a4a74faf6b5fea0ddc95d129d192b
Nat <nat.weerawan@gmail.com>
2012-05-29 10:28:12 +0700

    Add test case.

--- test/unit/th/co/opendream/cashcard/PeriodGeneratorProcessorServiceTests.groovy
+++ test/unit/th/co/opendream/cashcard/PeriodGeneratorProcessorServiceTests.groovy
@@ -11,65 +11,37 @@ import org.junit.*
 @TestFor(PeriodGeneratorProcessorService)
 @Mock([LoanType, Member, Period, Contract])
 class PeriodGeneratorProcessorServiceTests {
+    def loanType
+
     @Before
     void setUp() {
-    }
-
-    /***********************************
-     * Effective
-     **********************************/
-    void setUpPeriod(type='Effective') {
-        def loanType = new LoanType(name: 'Common', processor: type)
-        loanType.save()
-
-        def member = new Member(identificationNumber:"1159900100015", firstname:"Nat", lastname: "Weerawan", telNo: "111111111", gender: "MALE", address: "Opendream")
-        member.utilService = [
-            check_id_card: { id -> true }
-        ]
-        member.save()
-
-        def contract = new Contract(
-            id: 1,
-            code: "ก.55-1000-20",
-            member: member,
-            loanType: loanType,
-            loanAmount: 2000.00,
-            cooperativeShare: 0.75,
+        loanType = new LoanType(
+            name: 'Common',
+            processor: 'Effective',
             interestRate: 24.00,
-            loanBalance: 2000.00,
-            approvalStatus: false,
-            loanReceiveStatus: false,
-            guarantor1: "Keng",
-            guarantor2: "Neung",
             numberOfPeriod: 3
-        )
-        contract.save()
-
-        mockDomain(Period, [
-            [id: 1, contract: contract, amount: 706.00, no: 1,
-             dueDate: new Date().plus(10), status: true, payoffStatus: true],
-            [id: 2, contract: contract, amount: 706.00, no: 2,
-             dueDate: new Date().plus(20), status: true, payoffStatus: false],
-            [id: 3, contract: contract, amount: 708.00, no: 3,
-             dueDate: new Date().plus(30), status: true, payoffStatus: false]
-        ])
+        ).save()
     }
 
-/*
     void testEffective() {
         def amount = 2000,
             numberOfPeriod = 3;
-        def periodList = service.effective(amount, numberOfPeriod)
-        periodList.each {
-            println it.dump()
-        }
+        def periodList = service.effective(amount, numberOfPeriod, loanType.interestRate)
     }
-    */
+
+    void testCommission() {
+        def amount = 2000,
+            numberOfPeriod = 3;
+        def periodList = service.commission(amount, numberOfPeriod, loanType.interestRate)
+    }
+
+    void testFlat() {
+        def amount = 2000,
+        numberOfPeriod = 3;
+        def periodList = service.flat(amount, numberOfPeriod, loanType.interestRate)
+    }
 
     void testGenerateEffective() {
-        def type = 'Effective'
-        def loanType = new LoanType(name: 'Common', processor: 'Effective', interestRate: 24.00)
-
         def amount = 2000,
             numberOfPeriod = 3;
 
@@ -77,6 +49,10 @@ class PeriodGeneratorProcessorServiceTests {
         def generated = service.generate(loanType, amount, numberOfPeriod)
         def effected = service.effective(amount, numberOfPeriod, loanType.interestRate)
 
+        assert (generated*.amount).equals(effected*.amount)
+
+        // Test With Id
+        generated = service.generate(loanType.id as Integer, amount, numberOfPeriod)
         assert (generated*.amount).equals(effected*.amount)
     }
 }

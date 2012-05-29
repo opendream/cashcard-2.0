3c7a14c7eb2366a2fa4cd81756ec39e18cb5c6bc
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-05-29 15:41:50 +0700

    created report controller

---
+++ grails-app/controllers/th/co/opendream/cashcard/ReportController.groovy
@@ -0,0 +1,33 @@
+package th.co.opendream.cashcard
+
+class ReportController {
+
+    def overdue() { 
+    	render(view:'overdue')
+    }
+
+    def doPaymentOverdueReport() {
+        println params
+        if(params.selectedDate) {
+    	   params.selectedDate = params.selectedDate.format("yyyy-MM-dd'T'HH:mm:ss'Z'")  
+           println params.selectedDate  	
+    	   chain(controller:'jasper',action:'index',params:params)
+        } else {
+           render(view:'overdue')
+        }
+    }
+
+    def payment() {  
+    	render(view:'payment', model:[type:params.paymenttype])
+    }
+
+    def doPaymentReport() { 
+        if(params.fromDate <= params.toDate) {  	
+    	   params.fromDate = params.fromDate.format("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    	   params.toDate = params.toDate.next().format("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    	   chain(controller:'jasper',action:'index',params:params)
+        } else {
+           render(view:'payment', model:[type:params._file]) 
+        }
+    }
+}

aefd5adfb262e2c54a1947573d83939ee9919764
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-05-31 11:19:35 +0700

    dirty fix for date.format()

--- grails-app/controllers/th/co/opendream/cashcard/ReportController.groovy
+++ grails-app/controllers/th/co/opendream/cashcard/ReportController.groovy
@@ -12,7 +12,10 @@ class ReportController {
             params._name = params.name
             params._file = params.file   
             params._format = params.format
-    	    params.selectedDate = params.selectedDate.format("yyyy-MM-dd'T'HH:mm:ss'Z'")       	
+    	    params.selectedDate = params.selectedDate.format("yyyy-MM-dd'T'HH:mm:ss'Z'") 
+            // fixed for demo
+            params.selectedDate = "$params.selectedDate_year-$params.selectedDate_month-$params.selectedDate_day"
+              	
     	    chain(controller:'jasper',action:'index',params:params)
         } else {
             render(view:'overdue')
@@ -24,16 +27,26 @@ class ReportController {
     }
 
     def doPaymentReport() {
-        //println params 
+         
         if(params.fromDate <= params.toDate) { 
             params._name = params.name
             params._file = params.file	
             params._format = params.format
-    	    params.fromDate = params.fromDate.format("yyyy-MM-dd'T'HH:mm:ss'Z'")
-    	    params.toDate = params.toDate.next().format("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    	    //params.fromDate = params.fromDate.format("yyyy-MM-dd'T'HH:mm:ss'Z'")
+            //params.toDate = params.toDate.next().format("yyyy-MM-dd'T'HH:mm:ss'Z'")
+            // fixed for demo
+            params.fromDate = "$params.fromDate_year-$params.fromDate_month-$params.fromDate_day"
+    	    params.toDate = "$params.toDate_year-$params.toDate_month-$params.toDate_day"
+            
     	    chain(controller:'jasper',action:'index',params:params)
         } else {
             render(view:'payment', model:[type:params._file]) 
         }
+    }
+
+    def parseDateToString(def date) {
+        def calendar = Calendar.getInstance(new Locale('en', 'US'))
+        calendar.setTime(date)
+        calendar.format('yyyy-MM-dd')
     }
 }

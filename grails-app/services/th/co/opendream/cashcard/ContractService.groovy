d508a9da2520df6cc982cae282eb16e8326547bc
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-06-12 17:40:48 +0700

    Add method for retrieve data no close contract action (clear balance)

--- grails-app/services/th/co/opendream/cashcard/ContractService.groovy
+++ grails-app/services/th/co/opendream/cashcard/ContractService.groovy
@@ -2,6 +2,10 @@ package th.co.opendream.cashcard
 
 class ContractService {
 
+    // Services
+    def interestProcessorService,
+        utilService
+
     def copyLoanProperties(contract, loanType) {
         contract.processor = loanType.interestProcessor
         contract.interestProcessor = loanType.interestProcessor
@@ -11,5 +15,25 @@ class ContractService {
         contract.maxInterestRate = loanType.maxInterestRate
 
         contract
+    }
+
+    def getInterestAmountOnCloseContract(period, paymentDate) {
+        def periodInterest = interestProcessorService.process(period, paymentDate)
+
+        def currentInterest
+        if (! period.interestPaid) {
+            currentInterest = period.interestOutstanding
+        }
+        else {
+            currentInterest = periodInterest.actualInterest
+        }
+
+        return [
+            totalDebt: period.contract.loanBalance + currentInterest,
+            loanBalance: period.contract.loanBalance,
+            goalInterest: period.interestOutstanding,
+            realInterest: periodInterest.actualInterest,
+            callInterest: currentInterest
+        ]
     }
 }
